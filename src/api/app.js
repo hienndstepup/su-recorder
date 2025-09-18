5d28a16865252fb1ecfa141d120eafa7d42bc533
hiennd <hiennd.tech@stepup.edu.vn>
2025-09-18 10:31:38 +0700

    Add new endpoint for English-Vietnamese ASR in app API

--- src/api/app.js
+++ src/api/app.js
@@ -2,12 +2,25 @@ import { apiClient } from "./index";
 
 const appEndpoints = {
   CHECK_ASR: "/speech/api/v1/check/tofu-open/asr_en",
+  CHECK_ASR_EN_VN: "/tofu/api/v1/tofu-open/chat_bot/asr",
 };
 
 export const appApi = {
   checkAsr: async (formData) => {
     const response = await apiClient.public.post(
       appEndpoints.CHECK_ASR,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return response;
+  },
+  checkAsrEnVn: async (formData) => {
+    const response = await apiClient.public.post(
+      appEndpoints.CHECK_ASR_EN_VN,
       formData,
       {
         headers: {
